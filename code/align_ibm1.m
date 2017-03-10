2317cc24cfa3ef906a6f434fb9fa6ea04e296be2
Quan Eddie <quaneddi@teach.cs.toronto.edu>
2017-03-09 19:06:47 -0500

    added more shit to align_ibm1

--- code/align_ibm1.m
+++ code/align_ibm1.m
@@ -35,26 +35,22 @@ function AM = align_ibm1(trainDir, numSentences, maxIter, fn_AM)
   
   % Read in the training data
   [eng, fre] = read_hansard(trainDir, numSentences);
-
-  % Initialize AM uniformly 
+  
+  % Initialize AM uniformly
   AM = initialize(eng, fre);
-
+  
   % Iterate between E and M steps
   for iter=1:maxIter,
-    AM = em_step(AM, eng, fre);
+      AM = em_step(AM, eng, fre);
   end
-
+  
   % Save the alignment model
-  save( fn_AM, 'AM', '-mat'); 
-
-  end
-
-
-
-
+  save( fn_AM, 'AM', '-mat');
+  
+end
 
 % --------------------------------------------------------------------------------
-% 
+%
 %  Support functions
 %
 % --------------------------------------------------------------------------------
@@ -67,16 +63,36 @@ function [eng, fre] = read_hansard(mydir, numSentences)
 %
 % Remember that the i^th line in fubar.e corresponds to the i^th line in fubar.f
 % You can decide what form variables 'eng' and 'fre' take, although it may be easiest
-% if both 'eng' and 'fre' are cell-arrays of cell-arrays, where the i^th element of 
+% if both 'eng' and 'fre' are cell-arrays of cell-arrays, where the i^th element of
 % 'eng', for example, is a cell-array of words that you can produce with
 %
 %         eng{i} = strsplit(' ', preprocess(english_sentence, 'e'));
 %
-  %eng = {};
-  %fre = {};
+eng = {};
+fre = {};
 
-  % TODO: your code goes here.
+% TODO: your code goes here.
+ENG_DD = dir( [ mydir, filesep, '*', 'e'] );
+FRE_DD = dir( [ mydir, filesep, '*', 'f'] );
 
+lines_written = 1;
+
+for iFile=1:length(ENG_DD)
+    english_lines = textread([mydir, filesep, ENG_DD(iFile).name], '%s','delimiter','\n');
+    french_lines = textread([mydir, filesep, FRE_DD(iFile).name], '%s','delimiter','\n');
+
+    for l=1:length(english_lines)
+        
+        eng{lines_written} = strsplit(' ', preprocess(english_lines{l}, 'e'));
+        fre{lines_written} = strsplit(' ', preprocess(french_lines{l}, 'f'));
+
+        lines_written = lines_written + 1;
+
+        if (lines_written > numSentences)
+          return;
+        end
+    end
+end
 end
 
 
@@ -86,8 +102,41 @@ function AM = initialize(eng, fre)
 % Only set non-zero probabilities where word pairs appear in corresponding sentences.
 %
     AM = {}; % AM.(english_word).(foreign_word)
+    AM.SENTSTART.SENTSTART = 1;
+    AM.SENTEND.SENTEND = 1;
 
     % TODO: your code goes here
+    % initialize AM english words
+    for sentence=1:length(eng)
+        for eng_word_index=2:length(eng{sentence})-1
+            for fre_word_index=2:length(fre{sentence})-1
+                eng_word = eng{sentence}{eng_word_index}
+                fre_word = fre{sentence}{fre_word_index}
+                
+                if ~isfield(AM, eng_word)
+                    AM.(eng_word) = struct();
+                end
+                
+                if ~isfield(AM.(eng_word), fre_word)
+                    AM.(eng_word).(fre_word) = 0;
+                end
+                
+            end
+        end
+    end
+    
+    eng_words = fieldnames(AM);
+    
+    for word=1:length(eng_words)
+        possible_pairs = fieldnames(AM.(eng_words{word}));
+        num_french_pairs = length(possible_pairs);
+        
+        for french_word_index=1:num_french_pairs
+            eng_word = eng_words{word};
+            fre_word = possible_pairs{french_word_index};
+            AM.(eng_word).(fre_word) = rdivide(1, num_french_pairs);
+        end
+    end
 
 end
 
